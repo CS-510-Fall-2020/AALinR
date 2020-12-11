bbdb1eaa4532ea613e1348252b8988252f4e3c92
niko.todorov <todorov@chapman.edu>
2020-12-11 00:28:05 -0800

    addressing the peer review comments

--- README.md
+++ README.md
@@ -15,9 +15,17 @@ Future enhancements:
 - Random RP losses
 - missing, erroneous, outlier data detection/correction
 
-Why AAL? A: because it is a good measure for relative natural hazards risk of a geographic area.
+Q: How is AAL different from the 1 return period (RP) loss?
+A: The AAL is the mean value of a loss exceedance probability (EP) distribution. It is the expected loss per year, averaged over many years. The one-year return period loss has 100% chance of occurance and is expected to be at least equaled every year.
 
-What is the ID? A: it could be a Census Block (CB), Census Tract (CT), County (CO), State (ST), or individual buildings, for which may have to use Google Plus codes.
+Q: What is the RP loss relationship w.r.t. the probability of occurance?
+A: The RP loss has inverse relationship w.r.t. probability of occurance. For example 100-year RP loss is said to equal 1/100 or 1% chance. Similarly, 10-year ~ 1/10 or 10% chance, 25-year ~ 1/25 or 4% chance, 500-year ~ 1/500 or 0.2% chance of occurance.
+
+Q: Why AAL? 
+A: because it is a good measure for relative natural hazards risk of a geographic area.
+
+Q: What is the ID? 
+A: it could be a Census Block (CB), Census Tract (CT), County (CO), State (ST), or individual buildings, for which may have to use Google Plus codes.
 
 Let me know your thoughts.
 
--- src/AALinR.R
+++ src/AALinR.R
@@ -1,46 +1,76 @@
+#########################################################
+#                                                       #
+#      Average Annualized Loss in R (AALinR)            #
+#                        by                             #
+#                   Niko Todorov                        #
+#                                                       #
+#########################################################
+install.packages("tidyr")
+install.packages("ggplot2")
+install.packages("ggthemes")
+install.packages("maps")
 library(tidyr)
 library(ggplot2)
 library(ggthemes)
 library(maps)
 # adjacent intervals
-C.10.20   = 0.025  # 0.5/10-0.5/20
-C.20.50   = 0.015  # 0.5/20-0.5/50
-C.50.100  = 0.005  # 0.5/50-0.5/100
-C.100.200 = 0.0025 # 0.5/100-0.5/200
-C.200.500 = 0.0015 # 0.5/200-0.5/500
-C.10.25   = 0.03   # 0.5/10-0.5/25
-C.25.50   = 0.01   # 0.5/25-0.5/50
-C.100.500 = 0.004  # 0.5/100-0.5/500
+C.10.20   <- 0.025  # 0.5/10-0.5/20
+C.20.50   <- 0.015  # 0.5/20-0.5/50
+C.50.100  <- 0.005  # 0.5/50-0.5/100
+C.100.200 <- 0.0025 # 0.5/100-0.5/200
+C.200.500 <- 0.0015 # 0.5/200-0.5/500
+C.10.25   <- 0.03   # 0.5/10-0.5/25
+C.25.50   <- 0.01   # 0.5/25-0.5/50
+C.100.500 <- 0.004  # 0.5/100-0.5/500
 
-# LA6 is simpler data
-LA6.norm = read.table("dat\\LA6.txt")
-colnames(LA6.norm) = c("CT", "RP", "Loss")
-LA6.norm[, 2] = sapply(LA6.norm[, 2], as.integer)
-LA6.norm[, 3] = sapply(LA6.norm[, 3], as.numeric)
-LA6.norm = LA6.norm [-1, ]
-LA6 = LA6.norm %>% pivot_wider(names_from = RP, values_from = Loss) # , values_fill = 0
+#########################################################
+# The LA6 hurricane loss dataset is an almost complete  #
+# dataset with no blanks/missing data, because it is    #
+# based on Census Tracts (CT) which are almost certainly#
+# guaranteed to contain a non-enpty Census Blocks (CB)  #
+# in terms of building exposure. Such completeness makes#
+# LA6 easy to compute the AAL utilizing the Rietmann    # 
+# sums numerical integration.                           #
+#########################################################
+LA6.norm <- read.table("dat\\LA6.txt")
+colnames(LA6.norm) <- c("CT", "RP", "Loss")
+LA6.norm[, 2] <- sapply(LA6.norm[, 2], as.integer)
+LA6.norm[, 3] <- sapply(LA6.norm[, 3], as.numeric)
+LA6.norm <- LA6.norm [-1, ]
+LA6 <- LA6.norm %>% pivot_wider(names_from <- RP, values_from <- Loss) # , values_fill <- 0
 
-LA6 = subset(LA6, select = c(1,2,4,5,6,7,8,3))
-LA6$`AAL`<-C.10.20*(LA6$`10`+LA6$`20`)+C.20.50*(LA6$`20`+LA6$`50`)+C.50.100*(LA6$`50`+LA6$`100`)+C.100.200*(LA6$`100`+LA6$`200`)+C.200.500*(LA6$`200`+LA6$`500`)
-ggplot(data = LA6.norm, mapping = aes(x=as.factor(RP), y=log(Loss), fill=as.factor(RP))) + geom_violin() # geom_point() # geom_contour
+LA6 <- subset(LA6, select <- c(1,2,4,5,6,7,8,3))
+LA6$`AAL`<-C.10.20*(LA6$`10`+LA6$`20`)
+          +C.20.50*(LA6$`20`+LA6$`50`)
+          +C.50.100*(LA6$`50`+LA6$`100`)
+          +C.100.200*(LA6$`100`+LA6$`200`)
+          +C.200.500*(LA6$`200`+LA6$`500`)
+ggplot(data <- LA6.norm, mapping <- aes(x<-as.factor(RP), y<-log(Loss), fill<-as.factor(RP))) + geom_violin() # geom_point() # geom_contour
+ggsave()
 
 # write.table(LA6, "dat\\LA6AAL.txt")
 # write.csv(LA6, "dat\\LA6.AAL.csv")
-# qplot(LA6$CT, LA6$AAL, data=LA6, geom=c("point","smooth"))
-# bp = ggplot(LA6.norm, aes(x=as.numeric(RP),y=as.numeric(CT)))
+# qplot(LA6$CT, LA6$AAL, data<-LA6, geom<-c("point","smooth"))
+# bp <- ggplot(LA6.norm, aes(x<-as.numeric(RP),y<-as.numeric(CT)))
 # bp
-
-
 # summary(LA6)
 
-NOLA.norm = read.table("dat\\NOLA.txt")
-colnames(NOLA.norm) = c("CB", "RP", "Loss")
-NOLA.norm[, 2] = sapply(NOLA.norm[, 2], as.integer)
-NOLA.norm[, 3] = sapply(NOLA.norm[, 3], as.numeric)
-NOLA.norm = NOLA.norm [-1, ]
-NOLA = NOLA.norm %>% pivot_wider(names_from = RP, values_from = Loss) # values_fill = 0
-NOLA = subset(NOLA, select = c(1,4,5,3,2,6))
-# do this later
+#########################################################
+# The NOLA flood loss dataset is sparse as compared to  #
+# the LA6 hurricane loss one. Considering such sparcity #
+# and ensuing consequent complexity of dynamic range    #
+# numerical integration using the Rietmann sums the     #
+# development of AAL methodology for NOLA dataset is    #
+# considered out-of-scope for the purposes of CS-510.   #
+#########################################################
+NOLA.norm <- read.table("dat\\NOLA.txt")
+colnames(NOLA.norm) <- c("CB", "RP", "Loss")
+NOLA.norm[, 2] <- sapply(NOLA.norm[, 2], as.integer)
+NOLA.norm[, 3] <- sapply(NOLA.norm[, 3], as.numeric)
+NOLA.norm <- NOLA.norm [-1, ]
+NOLA <- NOLA.norm %>% pivot_wider(names_from <- RP, values_from <- Loss) # values_fill <- 0
+NOLA <- subset(NOLA, select <- c(1,4,5,3,2,6))
+# TODO: NOLA later
 #NOLA$`AAL`<-C.10.25*(NOLA$`10`+NOLA$`25`)+C.25.50*(NOLA$`25`+NOLA$`50`)+C.50.100*(NOLA$`50`+NOLA$`100`)+C.100.500*(NOLA$`100`+NOLA$`500`)
-
-data(county.fips)
+# TODO: roll up CB/CT to and plot at county level
+#data(county.fips)
